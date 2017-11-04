4970218a4d14469cd424452db2e95127799c6803
Komahan Boopathy <komahan.cool@gmail.com>
2017-11-04 15:07:22 -0400

    added a basic version of list with append functionality

---
+++ list.f90
@@ -0,0 +1,128 @@
+module list_class
+
+  implicit none
+
+  type :: node
+     
+     class(*)  , allocatable :: data
+     type(node), allocatable :: next
+     
+   contains
+
+     !final :: destroy_node
+     
+  end type node
+
+  type :: list
+     
+     class(node), allocatable :: head
+     class(node), allocatable :: tail
+     type(integer)            :: length
+          
+   contains
+
+     procedure :: append
+     !final :: destroy_list
+
+  end type list
+
+  interface node
+     module procedure create_node
+  end interface node
+
+    interface list
+     module procedure create_list
+  end interface list
+
+contains
+
+  !===================================================================!
+  ! Constructor for node
+  !===================================================================!
+  
+  pure type(node) function create_node(data) result(this)
+    
+    class(*), intent(in) :: data
+
+    ! Use sourced allocation to determine datatype at runtime
+    allocate(this % data, source=data)
+
+    ! Set the next node to null
+    if (allocated(this % next)) deallocate(this % next)
+         
+  end function create_node
+
+  !===================================================================!
+  ! Instantiate an empty list
+  !===================================================================!
+  
+  type(list) function create_list() result(this)
+
+    if (allocated(this % head)) deallocate(this % head)
+    if (allocated(this % tail)) deallocate(this % tail)
+    this % length = 0
+    
+  end function create_list
+
+  !===================================================================!
+  ! Append the item to list of nodes
+  !===================================================================!
+  
+   subroutine append(this, item)
+
+     class(list), intent(inout) :: this     
+     class(*), intent(in) :: item
+     
+     type(node) :: newnode
+    
+     ! Create new node encapsulating the data
+     newnode = node(item)
+     
+     if (this % length .eq. 0) then
+
+        ! head, tail are the newly added node
+        allocate(this % tail, source = newnode)        
+        allocate(this % head, source = newnode)
+ 
+     else
+              
+        ! The next node of the previous tail node is the new node
+        allocate(this % tail % next, source = newnode)
+        
+        ! Release the pointer to old tail node
+        deallocate(this % tail)
+
+        ! Now point to new tail node
+        allocate(this % tail, source = newnode)
+
+     end if
+
+     ! The list has grown by 1
+     this % length = this % length + 1
+     
+  end subroutine append
+  
+end module list_class
+
+!=====================================================================!
+! Test program for list
+!=====================================================================!
+
+program test_list
+
+  use list_class
+
+  type(list) :: mylist
+  
+  mylist = list()
+
+  call mylist % append(1)
+  call mylist % append(2)
+  call mylist % append(1.2)
+  call mylist % append("komahan boopathy")
+  call mylist % append(mylist)
+  !call mylist % append(1)
+  
+  print *, mylist % length
+  
+end program test_list
